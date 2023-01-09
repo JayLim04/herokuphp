cd7ba144bc4d393a6f00b1f71d02367ab9246a88
Kittymittens11 <21033869@myrp.edu.sg>
2023-01-09 15:31:59 +0800

    Lighthouse ver 2

--- web/test.php
+++ web/test.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
-<title>
-Heroku test php
-</title>
 <head>
-<meta name="description" content="Heroku Test.">
-
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Heroku Test</title>
 </head>
 <body>
+
   Hello World!
 </body>
 </html>
